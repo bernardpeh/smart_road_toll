a2c7eb34950d406ae7d7c71001b057e1973cbc99
bernard peh <bpeh777@gmail.com>
2017-11-16 23:24:17 +1100

    refactoring

--- README.md
+++ README.md
@@ -8,11 +8,11 @@ Road toll Implementation can be quite varied, from human (manual) tolls to fully
 
 Assuming a country uses RFID devices for road toll. The way it works is:
 
-* All vehicles will have a RFID device installed when the vehicle is first purchased. The RFID ID is linked to a carplate in the regulator central database.
+* All vehicles will have a RFID device installed when the vehicle is first purchased. The RFID ID is linked to a carplate in the regulator's central database.
 
 * Driver tops up a cash card and inserts the card into the device when driving.
 
-* When the vehicle passes the toll, the card value is automatically deducted based on some business rules decided by the regulator.
+* When the vehicle passes the toll, the card value is automatically deducted based on some business rules decided by the Toll.
 
 * If the card does not have enough credit when passing the toll, the vehicle owner will be penalised.
 
@@ -27,19 +27,19 @@ Assuming a country uses RFID devices for road toll. The way it works is:
 
 * Driver does not know if toll is charging correctly. Driver has to trust the regulator as there is currently no way for the driver to access his own toll ledger.
 
-* Traffic is equally bad most of the time as the toll fee is too low and not responsive to traffic conditions. Regulator could have made the fees higher for everyone but it gets political.
+* Traffic is equally bad most of the time as the toll fee is too low and not responsive to traffic conditions. Regulator could have made the fees higher but it applies to everyone and it gets political.
 
 ## Possible Solution:
 
 * A permissionless blockchain to track all transactions for all vehicles passing through the toll.
 
-* Driver deposit credit against the vehicle's RFID id inside a smart contract. Once the vehicle passes through the toll, vehicle's credit amount is automatically debited inside the contract. It works very much like the cash card but without the card and cannot be stolen.
+* Driver deposit credit to his own account inside a smart contract. Once the vehicle passes the toll, driver's account is automatically debited inside the contract. It works very much like the cash card but without the card and cannot be stolen.
 
 * The smart contract allows user to check any toll fee at any point in time.
 
 * The fee is responsive to traffic conditions based on the number of vehicles between 2 toll points every minute, ie fees will be higher when there is a traffic jam and lower when traffic is low.
 
-* The smart contract allows user to pre-book a journey early. Driver pay a small fee and lock in the date/time of travel to get better toll rates. Please refer to the diagram below.
+* The smart contract allows user to pre-book a journey early. Driver pay a small fee and lock in the date/time of travel to get better toll rates. This will allow better traffic distribution throughout the day (refer to the diagram below).
 
 * It is possible to integrate the blockchain into a GPS device. This will provide a good UI for driver to calculate the the toll charges for the journey as well. 
 
@@ -47,14 +47,16 @@ Assuming a country uses RFID devices for road toll. The way it works is:
 
 ![Smart Toll](images/smart_toll.jpg)
 
-## Implementation
+## An Example
 
-* Sam is the owner of a truck (rfid: SATOSHI).
+* A new truck is manufactured. The regulator registers the truck in the Smart Toll Contract. Let's say the truck is given a unique vehicle address called 0xSATOSHI. This address is embedded into the rfid device of the truck. Together with this address, details of the truck such as manufactured date, type, model...etc is stored in an off-chain system.
 
-* Sam wants to use his blockchain wallet address 0x123 for all his payments.
+* Sam bought the truck and is now the owner of the new truck.
 
-* The regulator registers the 0x12345 to be the rightly owner of vehicle rfid "SATOSHI" in the smart contract.
+* Sam's sensitive details such as name, date of birth, license number, contact number and address is kept off-chain. The regulator creates a new account address in the blockchain and associates it to the off-chain account. Let's say Sam's unique on-chain account address is 0xSAM.
 
-* Sam tops up his vehicle balance in the contract.
+* In the smart contract, the regulator associates 0xSAM to be the rightful owner of vehicle 0xSATOSHI.
 
-* Sam starts driving 
+* Sam tops up his account in the blockchain.
+
+* Sam starts driving. When the truck passes through the toll, Sam's account "0xSAM" is auto debited.
--- SmartRoadToll.sol
+++ SmartRoadToll.sol
@@ -11,39 +11,40 @@ contract ACL {
     // allow regulator to update his own address
     function changeAdmin(address _new_admin) public AdminOnly() returns (bool success) {}
 
-    function() public { revert(); }
-
 }
 
 contract Driver is ACL {
 
-    bytes32 firstName;
-    bytes32 lastName;
-    bytes32 driverLicense;
-    bool banned;
-    uint balance;
+    // driver details like name and license number is stored off-chain for security reasons.
 
-    // initialises the user. Could have more attributes.
-    function Driver(bytes32 _first_name, bytes32 _last_name, bool _banned) public returns (bool success) {}
+    // decides if driver can drive on road or not.
+    bool banned;
+
+    // initialises the user. Could have more attributes if desired.
+    function Driver(bool _banned) public {}
 
     // Allow or disallow driver to drive.
-    function setBanned() public AdminOnly() returns (bool success) {}
+    function setBanned(bool _ban) public AdminOnly() returns (bool success) {}
 
     // Basically, a selfdestruct()
     function remove() public AdminOnly() returns (bool success) {}
 
-    function() public { revert(); }
+    // able to accept payment
+    function () public payable {}
 }
 
 contract Vehicle is ACL {
 
-    bytes32 rfid;
-    bool banned;
-    address vehicleOwner;
-    uint balance;
-    Driver driver;
+    bytes32 public carPlate;
+    bool public banned;
+    address public vehicleOwner;
+    // the driver at any point in time. Owner has to ensure if its anyone other than himself driving,
+    // this var needs to be updated.
+    address public driver;
+    // needs to be true when trip started. false when trip ended.
+    bool public tripStarted;
 
-    mapping (address => Driver) authorisedDrivers;
+    mapping (address => Driver) public authorisedDrivers;
 
     // ensure vehicle owner
     modifier VehicleOwnerOnly() { _; }
@@ -52,7 +53,7 @@ contract Vehicle is ACL {
     modifier AuthorisedDriversOnly() { _; }
 
     // Contructor to create the vehicle.
-    function Vehicle(bytes32 _rfid, address _vehicle_owner, bool _banned) public returns (bool success) {}
+    function Vehicle(bytes32 _rfid, address _vehicle_owner, bool _banned) public {}
 
     // allow admin to change vehicle owner
     function changeVehicleOwner(address _new_owner) public AdminOnly() returns (bool success) {}
@@ -61,16 +62,19 @@ contract Vehicle is ACL {
     function setBanned() public AdminOnly() returns (bool success) {}
 
     // vehicle owner can allow other drivers to drive the vehicle.
-    function addDriver(address _new_driver) public VehicleOwnerOnly() returns (bool success) {}
+    function addAuthorisedDriver(address _new_driver) public VehicleOwnerOnly() returns (bool success) {}
 
     // vehicle owner can remove other drivers from driving the vehicle.
-    function removeDriver(address _driver) public VehicleOwnerOnly() returns (bool success) {}
+    function removeAuthorisedriver(address _driver) public VehicleOwnerOnly() returns (bool success) {}
 
-    // allow drivers to top up the vehicle balance
-    function topUp() public AuthorisedDriversOnly() payable returns (bool success) {}
+    // vehicle owner to set one authorised driver for the new trip
+    function setDriver(address _driver) public VehicleOwnerOnly returns (bool success) {}
 
-    // get vehicle balance at any point in time
-    function getVehicleBalance() public returns (uint balance) {}
+    // get driver at any point in time
+    function getDriver() public returns (Driver current_driver) {}
+
+    // set trip started and ended
+    function setTripStarted(bool _tripStarted) public VehicleOwnerOnly returns (bool started) {}
 
     // Basically, a selfdestruct()
     function remove() public AdminOnly() returns (bool success) {}
@@ -80,19 +84,20 @@ contract Vehicle is ACL {
 }
 
 contract Toll is ACL {
+
     uint id;
     uint coordinates;
     // We can get really complicated with fee calculation. Lets just say its a flat fee at certain point in time.
-    mapping (uint => uint) fees;
+    uint fee;
 
     // constructor
-    function Toll(uint _id, uint _coordinates, uint _fee) public returns (bool success) {}
+    function Toll(uint _id, uint _coordinates, uint _fee) public {}
 
     // set Toll fee at certain point in time
     function setTollFee(uint _datetime, uint _fee) public returns (bool success) {}
 
-    // get toll fee at certain toll point at certain time
-    function getTollFee(uint _toll_id, uint _datetime) public returns (uint fee) {}
+    // get toll fee at certain toll point at certain time based on some logic.
+    function getTollFee(uint _toll_id, uint _datetime) public returns (uint current_fee) {}
 
     // Basically, a selfdestruct()
     function remove() public AdminOnly() returns (bool success) {}
@@ -100,43 +105,43 @@ contract Toll is ACL {
     function() public { revert(); }
 }
 
-contract SmartRoadToll {
-
-    Vehicle vehicle;
-    Driver driver;
-    Toll toll;
+contract SmartRoadToll is ACL {
 
     struct Journey {
         address driver;
-        bytes32 rfid;
+        address vehicle;
         uint datetime;
-        mapping (uint => toll) tollsPassed;
+        mapping (uint => Toll) tollsPassed;
         uint feeTotal;
     }
 
     mapping (uint => Journey) public bookedJourneys;
 
     // map vehicle address to driver
-    mapping (address => vehicle) public vehicles;
+    mapping (address => Vehicle) public vehicles;
 
     mapping (uint => Toll) public tolls;
 
-    mapping (address => driver) public drivers;
+    mapping (address => Driver) public drivers;
 
     // add new Driver to the system
-    function registerDriver(bytes32 _first_name, bytes32 _last_name, bool _banned) public AdminOnly() returns (bool success);
+    function registerDriver(bool _banned) public AdminOnly() returns (address driver_address) {}
 
-    function registerVehicle(bytes32 _rfid, address _vehicle_owner, bool _banned) public AdminOnly() returns (bool success);
+    // add new vehicle to the system
+    function registerVehicle(bytes32 _rfid, address _vehicle_owner, bool _banned) public AdminOnly() returns (address vehicle_address) {}
+
+    // add new toll to the system
+    function registerToll(uint _id, uint _coordinates) public AdminOnly() returns (address toll_address) {}
 
     // for users who pre-book, require datetime of travel. Small payment will be deducted automatically from driver balance.
-    function bookJourney(bytes32 _rfid, uint _datetime, uint[] _tollsPassed) public returns (uint book_id) {}
+    function bookJourney(address _vehicle, address _driver, uint _datetime, uint[] _tollsPassed) public returns (uint book_id) {}
 
     // unbook journey
     function unbookJourney(uint _book_id) public returns (bool success) {}
 
     // Driver account automatically debited when vehicle passes through the toll.
     // There will also be logic here to automatically update toll fee based on traffic conditions.
-    function chargeVehicle(bytes32 _rfid, uint _toll_id, uint _datetime) public returns (bool success) {}
+    function payToll(address _vehicle, uint _toll_id, uint _datetime) public returns (bool success) {}
 
     function() public { revert(); }
 }
